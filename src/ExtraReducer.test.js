9847812cfc22350833da9ffd1dbe474e00dd8b8c
youliangdao <youliangdaojing5@gmail.com>
2022-12-08 11:25:26 +0900

    🚨test: ExtraReducerのテスト

---
+++ src/ExtraReducer.test.js
@@ -0,0 +1,23 @@
+import reducer, {
+  fetchDummy,
+  fetchJSON,
+} from "./features/customCounter/customCounterSlice";
+
+describe("customCounterSliceに登録されたextraReducerのテスト", () => {
+  const initialState = {
+    mode: 0,
+    value: 0,
+    username: "",
+  };
+
+  it("fetchDummyが正常終了した場合に、valueが100 + payloadになることを確認", () => {
+    const action = { type: fetchDummy.fulfilled.type, payload: 5 };
+    const newState = reducer(initialState, action);
+    expect(newState.value).toBe(105);
+  });
+  it("fetchDummyがエラーになった場合に、valueが100 - payloadになることを確認", () => {
+    const action = { type: fetchDummy.rejected.type, payload: 5 };
+    const newState = reducer(initialState, action);
+    expect(newState.value).toBe(95);
+  });
+});
